75cbe8c001c69e79bda385a26424b38ffe22b0f5
RodrigoRocS <rrsilva1701@gmail.com>
2023-04-19 15:53:19 -0300

    requisito 1 completo

---
+++ src/pages/Login.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  state = {
+    nome: '',
+    email: '',
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  };
+
+  render() {
+    const { nome, email } = this.state;
+    const isValid = this.validateEmail(email) && nome.length > 0;
+
+    return (
+      <div>
+        <form action="">
+          <label htmlFor="nome">
+            <input
+              type="text"
+              data-testid="input-player-name"
+              name="nome"
+              value={ nome }
+              onChange={ this.onInputChange }
+              placeholder="Digite seu nome"
+            />
+          </label>
+          <label htmlFor="email">
+            <input
+              type="email"
+              name="email"
+              id=""
+              data-testid="input-gravatar-email"
+              value={ email }
+              onChange={ this.onInputChange }
+              placeholder="Digite seu e-mail"
+            />
+          </label>
+          <button data-testid="btn-play" disabled={ !isValid }>Play</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
